5256cceceb438838c154b13a0b42bb9f6336ec26
Anders Feldinger <af@andersfeldinger.dk>
2015-05-19 23:44:59 +0200

    rearranged image folder structure

--- gruntfile.js
+++ gruntfile.js
@@ -159,7 +159,7 @@ module.exports = function(grunt) {
       options: {
         //prefix : 'icon-', // This will prefix each ID
         svg: { // will add and overide the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
-          viewBox : '0 0 100 100',
+          //viewBox : '0 0 100 100',
           xmlns: 'http://www.w3.org/2000/svg'
         },
         formatting: {
